81f0f5ea5bb54dd5e9ac944ad41486f68f49a3ea
Darren2017 <17362990052@163.com>
2018-04-29 21:46:32 +0800

    s

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -193,14 +193,14 @@ int bitCount(int x) {
  *   Max ops: 12
  *   Rating: 4 
  */
-int bang(int x) {         //未完成----------------------------
+int bang(int x) { 
   x = ~ x;
   x = (x & 0x55555555) + ((x >> 1) & 0x55555555);  
   x = (x & 0x33333333) + ((x >> 2) & 0x33333333);  
   x = (x & 0x0f0f0f0f) + ((x >> 4) & 0x0f0f0f0f);  
   x = (x & 0x00ff00ff) + ((x >> 8) & 0x00ff00ff);  
   x = (x & 0x0000ffff) + ((x >> 16) & 0x0000ffff);  
-  return (x >> 4) >> 2;
+  return x >> 5;
 }
 /* 
  * tmin - return minimum two's complement integer 
